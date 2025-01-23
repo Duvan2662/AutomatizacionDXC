7a17291c554b8715cdee7dcdd4fa98748f727a4a
Duvan <duvanriano2626@gmail.com>
2025-01-22 20:29:48 -0500

    Creacion de las rutas de mi app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'DXC',
+    loadComponent: () => import('./dashboard/dashboard.component').then(component => component.DashboardComponent),
+    children:[
+      {
+        path:'form',
+        title: 'Form',
+        loadComponent:() => import('./pages/formulario/formulario.component').then(component=>component.FormularioComponent),
+
+      },
+      {
+        path:'download',
+        title: 'Download',
+        loadComponent:() => import('./pages/formulario/formulario.component').then(component=>component.FormularioComponent),
+      },
+      {
+        path:'',
+        redirectTo:'form',
+        pathMatch:'full'
+      }
+
+    ]
+  },
+  {
+    path:'',
+    redirectTo: '/DXC',
+    pathMatch:'full'
+  }
+];
---
+++ src/app/pages/pantalla-final/pantalla-final.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-final',
+  imports: [],
+  templateUrl: './pantalla-final.component.html',
+  styleUrl: './pantalla-final.component.css'
+})
+export class PantallaFinalComponent {
+
+}
