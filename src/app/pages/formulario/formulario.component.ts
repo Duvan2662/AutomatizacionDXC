5b8b3af8550cb0a5a169ffbad999f43a9aad3c32
Duvan <duvanriano2626@gmail.com>
2025-01-27 06:17:35 -0500

    Se empiezan a colocar los inputs necesarios del formulario

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
 
+  constructor(private fb: FormBuilder) { }
+
+  public myform: FormGroup = this.fb.group({
+    testCase: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    password2: ['', [Validators.required]]
+  });
+
 }
+
+
+
+
+
